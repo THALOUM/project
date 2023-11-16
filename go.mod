c04b84eef60dff8648d08368827ffa618856e634
THALOUM <amr31hassan3@gmail.com>
2023-11-16 17:45:20 +0200

    import puppy module in the project dependancy and using Bark function from it

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/THALOUM/project
+
+go 1.21.4
+
+require github.com/THALOUM/Puppy v0.0.0-20231116121257-160d29ccedb8
---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+
+	puppy "github.com/THALOUM/Puppy"
+)
+
+func main() {
+	s1 := puppy.Bark()
+	fmt.Println(s1)
+}
